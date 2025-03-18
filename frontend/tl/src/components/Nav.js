78dd719445defe478a400637ee1400ada64d7279
Muhammed Cheema <cheemam0807@gmail.com>
2025-03-18 17:47:37 -0400

    edited routes for the logo and everything

--- frontend/tl/src/components/Nav.js
+++ frontend/tl/src/components/Nav.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import "./styling/Nav.css";
 import logo from '../images/logo.png';
+import { Link } from "react-router-dom";
 
 function Nav() {
     return (
         <nav className="Header">
             <div className="logo-section">
-                <img className="logo" src={logo} alt="Company Logo" />
+                <Link to="/">
+                    <img className="logo" src={logo} alt="Company Logo" />
+                </Link>
             </div>
-            <h1>TL General Construction</h1>
+
+            <Link to="/aboutus">
+                <h1>TL General Construction</h1>
+            </Link>
             <ul className="links">
-                <a href = "/">Home</a>
-                <a href ="/contact">Contact Us</a>
+                <Link to="/contact">Contact Us</Link>
             </ul>
         </nav>
     );
